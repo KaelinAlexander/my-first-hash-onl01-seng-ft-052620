45c2e1577fe3fd01877c261849e6035371ace564
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-01 13:26:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,10 @@
 def my_hash
-my_hash={
-
-  
+  my_hash={"cat" => "George", "dog" => "Keats"}
 end
 
 
 def shipping_manifest
+  the_manifest="whale bone corsets" => 5
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
